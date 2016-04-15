ea5ad36813efbb58cf16886374ef400059b9a6a0
AJ Apolinario <allenjamesxxiv@gmail.com>
2016-04-15 23:10:16 +0800

    update
    re group routes to admin in first prefix in routing alia url.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,75 +11,79 @@
 |
 */
 
-Route::get('/', array('uses' =>'HomeController@index', 'as' => 'home'));
-
-Route::group(array('before' => 'guest'), function()
+Route::group(array('prefix' => '/admin'),function()
 {
-	Route::get('/login',array('uses' =>'UserController@getLogin', 'as' => 'getLogin'));
-	Route::group(array('before' => 'csrf'), function()
+	Route::get('/', array('uses' =>'HomeController@index', 'as' => 'home'));
+
+	Route::group(array('before' => 'guest'), function()
 	{
-		Route::post('/user/create', array('uses' => 'UserController@postCreate', 'as' => 'postCreate'));
-		Route::post('/user/login',array('uses' => 'UserController@postLogin', 'as' => 'postLogin')); 
-	});
-});
-
-Route::group(array('before' => 'auth'), function()
-{
-	Route::get('/user/logout', array('uses' => 'UserController@getLogout', 'as' => 'getLogout'));
-});
-
-Route::group(array('prefix' => '/ajax'),function()
-{
-	Route::group(array('before' => 'auth'), function()
-	{
+		Route::get('/login',array('uses' =>'UserController@getLogin', 'as' => 'getLogin'));
 		Route::group(array('before' => 'csrf'), function()
 		{
-			Route::get('/statsbox', array('uses' => 'GlobalController@statsbox', 'as' => 'statsbox','middleware' => 'auth'));
-			Route::get('/admin/list',array('uses' => 'UAMController@adminUserList', 'as' => 'adminUserList'));
-			Route::get('/admin/uaal', array('uses' => 'UAMController@uaal', 'as' => 'uaal','middleware' => 'auth'));
-			Route::get('/admin/information/{id}', array('uses' => 'GlobalController@userInfoList', 'as' => 'userInfoList','
-				middleware' => 'auth'));
-			Route::get('/admin/userlist', array('uses' => 'GlobalController@userlist', 'as' => 'userlist','
-				middleware' => 'auth'));
-			Route::get('/admin/accountAccessChecker/{event}', array('uses' => 'GlobalController@accountAccessChecker', 'as' => 'accountAccessChecker','
-				middleware' => 'auth'));
-			Route::get('/product/categoryList', array('uses' => 'FileMaintenanceController@categoryList', 'as' => 'categoryList','middleware' => 'auth'));
-			Route::get('/product/categoryInfo/{cid}', array('uses' => 'GlobalController@categoryInfo', 'as' => 'categoryInfo','middleware' => 'auth'));
-
-			Route::post('/product/addCategory', array('uses' => 'FileMaintenanceController@addCategory', 'as' => 'addCategory','middleware' => 'auth'));
+			Route::post('/user/create', array('uses' => 'UserController@postCreate', 'as' => 'postCreate'));
+			Route::post('/user/login',array('uses' => 'UserController@postLogin', 'as' => 'postLogin')); 
 		});
 	});
-});
 
-Route::group(array('prefix' => '/uam'),function()
-{
 	Route::group(array('before' => 'auth'), function()
 	{
-		Route::get('/getUAL', array('uses' => 'UAMController@getUAL', 'as' => 'getUAL','middleware' => 'auth'));
-		Route::get('/getRoles', array('uses' => 'UAMController@getRoles', 'as' => 'getRoles','middleware' => 'auth'));
-		Route::get('/getPermissions', array('uses' => 'UAMController@getPermissions', 'as' => 'getPermissions','middleware' => 'auth'));
+		Route::get('/user/logout', array('uses' => 'UserController@getLogout', 'as' => 'getLogout'));
 	});
-});
 
-Route::group(array('prefix' => '/filemaintenance'),function()
-{
-	Route::group(array('before' => 'auth'), function()
+	Route::group(array('prefix' => '/ajax'),function()
 	{
-		Route::get('/getCategory', array('uses' => 'FileMaintenanceController@getCategory', 'as' => 'getCategory','middleware' => 'auth'));
-	});
-});
-
-Route::group(array('prefix' => '/product'),function()
-{
-	Route::group(array('before' => 'auth'), function()
-	{
-		Route::get('/', array('uses' => 'ProductController@getProductView', 'as' => 'getProductView','middleware' => 'auth'));
-		Route::group(array('before' => 'csrf'), function()
+		Route::group(array('before' => 'auth'), function()
 		{
-			Route::post('/addProduct', array('uses' => 'ProductController@addProduct', 'as' => 'addProduct','middleware' => 'auth'));
-			Route::post('/uploadProductImage', array('uses' => 'ProductController@uploadProductImage', 'as' => 'uploadProductImage','middleware' => 'auth'));
-			Route::post('/deleteImage', array('uses' => 'ProductController@deleteImage', 'as' => 'deleteImage','middleware' => 'auth'));
-			
+			Route::group(array('before' => 'csrf'), function()
+			{
+				Route::get('/statsbox', array('uses' => 'GlobalController@statsbox', 'as' => 'statsbox','middleware' => 'auth'));
+				Route::get('/admin/list',array('uses' => 'UAMController@adminUserList', 'as' => 'adminUserList'));
+				Route::get('/admin/uaal', array('uses' => 'UAMController@uaal', 'as' => 'uaal','middleware' => 'auth'));
+				Route::get('/admin/information/{id}', array('uses' => 'GlobalController@userInfoList', 'as' => 'userInfoList','
+					middleware' => 'auth'));
+				Route::get('/admin/userlist', array('uses' => 'GlobalController@userlist', 'as' => 'userlist','
+					middleware' => 'auth'));
+				Route::get('/admin/accountAccessChecker/{event}', array('uses' => 'GlobalController@accountAccessChecker', 'as' => 'accountAccessChecker','
+					middleware' => 'auth'));
+				Route::get('/product/categoryList', array('uses' => 'FileMaintenanceController@categoryList', 'as' => 'categoryList','middleware' => 'auth'));
+				Route::get('/product/categoryInfo/{cid}', array('uses' => 'GlobalController@categoryInfo', 'as' => 'categoryInfo','middleware' => 'auth'));
+
+				Route::post('/product/addCategory', array('uses' => 'FileMaintenanceController@addCategory', 'as' => 'addCategory','middleware' => 'auth'));
+			});
 		});
 	});
+
+	Route::group(array('prefix' => '/uam'),function()
+	{
+		Route::group(array('before' => 'auth'), function()
+		{
+			Route::get('/getUAL', array('uses' => 'UAMController@getUAL', 'as' => 'getUAL','middleware' => 'auth'));
+			Route::get('/getRoles', array('uses' => 'UAMController@getRoles', 'as' => 'getRoles','middleware' => 'auth'));
+			Route::get('/getPermissions', array('uses' => 'UAMController@getPermissions', 'as' => 'getPermissions','middleware' => 'auth'));
+		});
+	});
+
+	Route::group(array('prefix' => '/filemaintenance'),function()
+	{
+		Route::group(array('before' => 'auth'), function()
+		{
+			Route::get('/getCategory', array('uses' => 'FileMaintenanceController@getCategory', 'as' => 'getCategory','middleware' => 'auth'));
+		});
+	});
+
+	Route::group(array('prefix' => '/product'),function()
+	{
+		Route::group(array('before' => 'auth'), function()
+		{
+			Route::get('/', array('uses' => 'ProductController@getProductView', 'as' => 'getProductView','middleware' => 'auth'));
+			Route::group(array('before' => 'csrf'), function()
+			{
+				Route::post('/addProduct', array('uses' => 'ProductController@addProduct', 'as' => 'addProduct','middleware' => 'auth'));
+				Route::post('/uploadProductImage', array('uses' => 'ProductController@uploadProductImage', 'as' => 'uploadProductImage','middleware' => 'auth'));
+				Route::post('/deleteImage', array('uses' => 'ProductController@deleteImage', 'as' => 'deleteImage','middleware' => 'auth'));
+				
+			});
+		});
+	});
+
 });
