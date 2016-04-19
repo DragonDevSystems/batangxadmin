c8e91eec6887e4281028b4cebb58ef29080981f8
jhunel <jhunel_2389@yahoo.com>
2016-04-19 13:15:53 -0700

    update
    update

---
+++ resources/views/customer/includes/newProduct.blade.php
@@ -0,0 +1,65 @@
+<div class="content_top">
+	<div class="heading">
+		<h3>New Products</h3>
+	</div>
+	<div class="see">
+		<p><a href="#">See all Products</a></p>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="section group">
+	<div class="grid_1_of_4 images_1_of_4">
+		 <a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/feature-pic1.jpg" alt="" /></a>
+		 <h2>Lorem Ipsum is simply </h2>
+		<div class="price-details">
+	       <div class="price-number">
+				<p><span class="rupees">$620.87</span></p>
+		    </div>
+		       		<div class="add-cart">								
+						<h4><a href="preview.html">Add to Cart</a></h4>
+				     </div>
+				 <div class="clear"></div>
+		</div>
+		 
+	</div>
+	<div class="grid_1_of_4 images_1_of_4">
+		<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/feature-pic2.jpg" alt="" /></a>
+		 <h2>Lorem Ipsum is simply </h2>
+		<div class="price-details">
+	       <div class="price-number">
+				<p><span class="rupees">$899.75</span></p>
+		    </div>
+		       		<div class="add-cart">								
+						<h4><a href="preview.html">Add to Cart</a></h4>
+				     </div>
+				 <div class="clear"></div>
+		</div>
+	    
+	</div>
+	<div class="grid_1_of_4 images_1_of_4">
+		<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/feature-pic3.jpg" alt="" /></a>
+		 <h2>Lorem Ipsum is simply </h2>
+		 <div class="price-details">
+	       <div class="price-number">
+				<p><span class="rupees">$599.00</span></p>
+		    </div>
+		       		<div class="add-cart">								
+						<h4><a href="preview.html">Add to Cart</a></h4>
+				     </div>
+				 <div class="clear"></div>
+		</div>
+	</div>
+	<div class="grid_1_of_4 images_1_of_4">
+		<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/feature-pic4.jpg" alt="" /></a>
+		 <h2>Lorem Ipsum is simply </h2>
+		<div class="price-details">
+	       <div class="price-number">
+				<p><span class="rupees">$679.87</span></p>
+		    </div>
+		       		<div class="add-cart">								
+						<h4><a href="preview.html">Add to Cart</a></h4>
+				     </div>
+				 <div class="clear"></div>
+		</div>				     
+	</div>
+</div>
