a2b3ccaf0012b0e3a0beadc4836caca5667f20fe
Jerbey <jerbeycapoquian@outlook.com>
2016-05-03 15:50:05 +0800

    update

--- resources/views/customer/user/myaccount.blade.php
+++ resources/views/customer/user/myaccount.blade.php
@@ -12,22 +12,50 @@
 	</div>
 	<div class="main">
 		<div class="content">
-			<div class="section group">
-				<div class="col span_2_of_3">
-					<div class="contact-form">
-						<div class="row">
-							<div class="col-xs-12 table-responsive">
-								<table class="table table-striped invoice-list">
-									<thead>
-										<tr>
-											<th>Invoice No.</th>
-											<th>Date</th>
-											<th>Customer Name</th>
-											<th>Status</th>
-										</tr>
-									</thead>
-									<tbody></tbody>
-								</table>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="section group">
+						<div class="col span_2_of_3">
+							<div class="contact-form">
+							  	<h2>My Account</h2>
+							    <form method="post" action="">
+							    	<div>
+								    	<span><label>Lastname</label></span>
+								    	<span><input name="lastname" value="{{$userInfo['lname']}}" type="text" class="textbox" required></span>
+								    </div>
+								    <div>
+								    	<span><label>Firstname</label></span>
+								    	<span><input name="firstname" value="{{$userInfo['fname']}}" type="text" class="textbox" required></span>
+								    </div>
+								    <div>
+								    	<span><label>E-mail</label></span>
+								    	<span><input name="email" value="{{$userInfo['email']}}" type="email" class="textbox" required></span>
+								    </div>
+								  <input type="hidden" value="{{ csrf_token() }}" name="_token">
+							    </form>
+							</div>
+		  				</div>
+		  			</div>
+				</div>
+				<div class="col-md-6">
+					<div class="section group">
+						<div class="col span_2_of_3">
+							<div class="contact-form">
+								<div class="row">
+									<div class="col-xs-12 table-responsive">
+										<table class="table table-striped invoice-list">
+											<thead>
+												<tr>
+													<th>Invoice No.</th>
+													<th>Date</th>
+													<th>Customer Name</th>
+													<th>Status</th>
+												</tr>
+											</thead>
+											<tbody></tbody>
+										</table>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
