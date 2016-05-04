f9efd6bad4c0918ed6f7cb4c242e2c7a9533d96c
deancalera <dean.calera@yahoo.com>
2016-05-04 10:08:38 -0700

    update
    css

--- resources/views/customer/includes/productSlider.blade.php
+++ resources/views/customer/includes/productSlider.blade.php
@@ -4,46 +4,44 @@
 	        <div id="mover">
 	        	<div id="slide-1" class="slide">			                    
 					<div class="slider-img">
-						<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-1-image.png" alt="learn more" /></a>									    
+						<a href="javascript:void(0)"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-1-image.png" alt="learn more" /></a>									    
 						</div>
 							<div class="slider-text">
-						 <h1>Game Extreme Sale<br><span>SALE</span></h1>
+						 <h1>Game Extreme<br><span>SALE</span></h1>
 						 <h2>UPTo 20% OFF</h2>
 						<div class="features_list">
 							<h4>Come and vist our site</h4>							               
 						</div>
-						 <a href="preview.html" class="button">Shop Now</a>
 						</div>			               
 						<div class="clear"></div>				
 					</div>	
 					<div class="slide">
 						<div class="slider-text">
-						<h1>Game Extreme Sale<br><span>SALE</span></h1>
+						<h1>Game Extreme<br><span>SALE</span></h1>
 						<h2>UPTo 40% OFF</h2>
 						<div class="features_list">
 						<h4>Come and vist our site</h4>							               
 						</div>
-						<a href="preview.html" class="button">Shop Now</a>
 						</div>		
 						 <div class="slider-img">
-						<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-3-image.jpg" alt="learn more" /></a>
+						<a href="javascript:void(0)"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-3-image.jpg" alt="learn more" /></a>
 						</div>						             					                 
 						<div class="clear"></div>				
 					</div>
-					<div class="slide">						             	
-						<div class="slider-img">
-						<a href="preview.html"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-2-image.jpg" alt="learn more" /></a>
-						</div>
-						<div class="slider-text">
-						<h1>Game Extreme Sale<br><span>SALE</span></h1>
-						<h2>UPTo 10% OFF</h2>
+					<div class="slide">
+						<div class="slider-text third">
+						<h1>Game Extreme<br><span>SALE</span></h1>
+						<h2>UPTo 40% OFF</h2>
 						<div class="features_list">
 						<h4>Come and vist our site</h4>							               
 						</div>
-						<a href="preview.html" class="button">Shop Now</a>
-						</div>	
+						</div>		
+						 <div class="slider-img third">
+						<a href="javascript:void(0)"><img src="{{env('FILE_PATH_CUSTOM')}}img/slide-2-image.jpg" alt="learn more" /></a>
+						</div>						             					                 
 						<div class="clear"></div>				
-					</div>												
+					</div>
+																
 	     </div>		
 	</div>
 	<div class="clear"></div>					       
