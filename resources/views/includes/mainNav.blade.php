034a58369876bf2e697c83cb14397139691063cb
Jhunel <jhunel_2389@yahoo.com>
2016-05-11 00:32:23 +0800

    update
    update

--- resources/views/includes/mainNav.blade.php
+++ resources/views/includes/mainNav.blade.php
@@ -28,39 +28,39 @@
         <li class="header">MAIN NAVIGATION</li>
         <li class="treeview {{ ($mt == "db") ? "active" : ""}}">
           <a href="{{ URL::Route('home') }}">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-dashboard"></i>--> <span>Dashboard</span> <i class="fa pull-right"></i>
           </a>
         </li> 
         <li class="treeview {{ ($mt == "uam") ? "active" : ""}}">
           <a href="{{ URL::Route('home') }}">
-            <i class="fa fa-users"></i> <span>User Access Management</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-users"></i>--> <span>User Access Management</span> <i class="fa pull-right"></i>
             <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li class="{{ $mt == "uam" ? (($cc == "ual") ? "active" : "") : ""}}"><a href="{{ URL::Route('getUAL') }}"><i class="fa fa-users"></i> User Admin Level</a></li>
-            <li class="{{ $mt == "uam" ? (($cc == "cl") ? "active" : "") : ""}}"><a href="{{ URL::Route('getCustomer') }}"><i class="fa fa-users"></i> Customer List</a></li>
+            <li class="{{ $mt == "uam" ? (($cc == "ual") ? "active" : "") : ""}}"><a href="{{ URL::Route('getUAL') }}"><!--<i class="fa fa-users"></i>--> User Admin Level</a></li>
+            <li class="{{ $mt == "uam" ? (($cc == "cl") ? "active" : "") : ""}}"><a href="{{ URL::Route('getCustomer') }}"><!--<i class="fa fa-users"></i>--> Customer List</a></li>
           </ul>
         </li>
 
         <li class="{{ ($mt == "fm") ? "active" : ""}} treeview">
           <a href="#">
-            <i class="fa fa-files-o"></i> <span>File Maintenance</span>
+            <!--<i class="fa fa-files-o"></i>--> <span>File Maintenance</span>
             <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
             <li class="{{ $mt == "fm" ? (($cc == "pc") ? "active" : "") : ""}}">
               <a href="{{ URL::Route('getCategory') }}">
-                <i class="fa fa-globe"></i> Product Category
+                <!--<i class="fa fa-globe"></i>--> Product Category
               </a>
             </li>
             <li class="{{ $mt == "fm" ? (($cc == "pt") ? "active" : "") : ""}}">
               <a href="{{ URL::Route('getProductView') }}">
-                <i class="fa fa-gamepad"></i> <span>Products</span> <i class="fa pull-right"></i>
+                <!--<i class="fa fa-gamepad"></i>--> <span>Products</span> <i class="fa pull-right"></i>
               </a>
             </li>  
             <li class="{{ $mt == "fm" ? (($cc == "dl") ? "active" : "") : ""}}">
               <a href="{{ URL::Route('getDeliveryView') }}">
-                <i class="fa fa-truck"></i> <span>Delivery</span> <i class="fa pull-right"></i>
+                <!--<i class="fa fa-truck"></i>--> <span>Delivery</span> <i class="fa pull-right"></i>
               </a>
             </li>
           </ul>
@@ -70,53 +70,53 @@
         
         <li class="{{ ($mt == "tp") ? "active" : ""}} treeview">
           <a href="#">
-            <i class="fa fa-files-o"></i> <span>Transactions</span>
+            <!--<i class="fa fa-files-o"></i>--> <span>Transactions</span>
             <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
             <li class="treeview {{ $mt == "tp" ? (($cc == "inv") ? "active" : "") : ""}}">
               <a href="{{ URL::Route('getInvoiceView') }}">
-                <i class="fa fa-file-text-o"></i> <span>Invoices</span> <i class="fa pull-right"></i>
+                <!--<i class="fa fa-file-text-o"></i>--> <span>Invoices</span> <i class="fa pull-right"></i>
               </a>
             </li>
 
             <li class="treeview {{ $mt == "tp" ? (($cc == "th") ? "active" : "") : ""}}">
               <a href="{{ URL::Route('getTransactionHistory') }}">
-                <i class="fa fa-file-text-o"></i> <span>Transaction History</span> <i class="fa pull-right"></i>
+                <!--<i class="fa fa-file-text-o"></i>--> <span>Transaction History</span> <i class="fa pull-right"></i>
               </a>
             </li>
             <li class="{{ $mt == "tp" ? (($cc == "wi") ? "active" : "") : ""}} treeview">
               <a href="{{ URL::Route('getWalkIn') }}">
-                <i class="fa fa-money"></i> <span>Walk-in</span> <i class="fa pull-right"></i>
+                <!--<i class="fa fa-money"></i>--> <span>Walk-in</span> <i class="fa pull-right"></i>
               </a>
             </li>
           </ul>
         </li>
         <li class="treeview {{ ($mt == "ml") ? "active" : ""}}">
           <a href="{{ URL::Route('getContactMailView') }}">
-            <i class="fa fa-envelope-o"></i> <span>Inquiries</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-envelope-o"></i>--> <span>Inquiries</span> <i class="fa pull-right"></i>
           </a>
         </li>
 
         <li class="treeview {{ ($mt == "int") ? "active" : ""}}">
           <a href="{{ URL::Route('getInventoryView') }}">
-            <i class="fa fa-file-text-o"></i> <span>Inventory</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-file-text-o"></i>--> <span>Inventory</span> <i class="fa pull-right"></i>
           </a>
         </li>
         <li class="treeview {{ ($mt == "cl") ? "active" : ""}}">
           <a href="{{ URL::Route('getCriLvlView') }}">
-            <i class="fa fa-file-text-o"></i> <span>Critical Level Item</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-file-text-o"></i>--> <span>Critical Level Item</span> <i class="fa pull-right"></i>
           </a>
         </li>
         <li class="treeview {{ ($mt == "sr") ? "active" : ""}}">
           <a href="{{ URL::Route('getSales') }}">
-            <i class="fa fa-file-text-o"></i> <span>Sales Report</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-file-text-o"></i>--> <span>Sales Report</span> <i class="fa pull-right"></i>
           </a>
         </li>
 
         <li class="treeview {{ ($mt == "nw") ? "active" : ""}}">
           <a href="{{ URL::Route('getNewsView') }}">
-            <i class="fa fa-newspaper-o"></i> <span>News</span> <i class="fa pull-right"></i>
+            <!--<i class="fa fa-newspaper-o"></i>--> <span>News</span> <i class="fa pull-right"></i>
           </a>
         </li>
       </ul>
